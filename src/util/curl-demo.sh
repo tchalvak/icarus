eb98e7b7e9ecc63fb1f0eb10c17a3fdfae4f106c
Roy R <tchalvakspam@gmail.com>
2019-01-25 09:46:54 -0500

    Add curl demo command for trivial is-up checking.

---
+++ src/util/curl-demo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Type the base api url, followed by [ENTER]: e.g. https://jpw7bvkdld.execute-api.us-east-1.amazonaws.com/dev/ "
+
+read base
+
+curl -G "${base}query" --data-urlencode 'query={greeting(firstName: "Icarus")}'
+curl -I "${base}query"
+
+echo -e "/nApi endpoint is available"
