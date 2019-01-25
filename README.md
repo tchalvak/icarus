d4183e249d0cbd21bd0b2822405596e63c4ca67d
Roy R <tchalvakspam@gmail.com>
2019-01-24 20:08:35 -0500

    Get serverless deploy of endpoint working.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Icarus
 Codename Icarus, APIGateway auto-deployment
+
+## Configure
+
+ - Set up an IAM user for serverless with the appropriate credentials: https://serverless.com/framework/docs/providers/aws/guide/credentials/
+(you may want to back down some of the so very high credentials in that account)
+ - Set up your aws-cli, I recommend setting it up with a profile specifically for serverless.
+ - `aws configure --profile serverless`
+
+## Install
+
+    yarn install
+    yarn test
+    npm install -g serverless
+
+## Deploy a serverless stack
+
+    serverless deploy
+
+## Additional info
+
+ Setting up a graphql api: https://serverless.com/blog/make-serverless-graphql-api-using-lambda-dynamodb/
---
+++ handler.js
@@ -0,0 +1,41 @@
+/*
+Set up a graphql with schema
+*/
+
+const {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull
+} = require('graphql')
+
+// A simple hello world functionality library
+// This method just inserts the user's first name into the greeting message.
+const getGreeting = firstName => `Hello, ${firstName}.`
+
+// Here we declare the schema and resolvers for the query
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'HelloQueryType', // an arbitrary name
+    fields: {
+      // the query has a field called 'greeting'
+      greeting: {
+        // we need to know the user's name to greet them
+        args: { firstName: { name: 'firstName', type: new GraphQLNonNull(GraphQLString) } },
+        // the greeting message is a string
+        type: GraphQLString,
+        // resolve to a greeting message
+        resolve: (parent, args) => getGreeting(args.firstName)
+      }
+    }
+  }),
+})
+
+// We want to make a GET request with ?query=<graphql query>
+// The event properties are specific to AWS. Other providers will differ.
+module.exports.query = (event, context, callback) => graphql(schema, event.queryStringParameters.query)
+  .then(
+    result => callback(null, {statusCode: 200, body: JSON.stringify(result)}),
+    err => callback(err)
+  )
