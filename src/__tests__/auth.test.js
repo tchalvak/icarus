35b07c88dc1274eadefe787bac51656d1a60dd97
Roy R <tchalvakspam@gmail.com>
2019-01-29 11:35:33 -0500

    Fix typo in auth tests.

--- src/__tests__/auth.test.js
+++ src/__tests__/auth.test.js
@@ -1,6 +1,6 @@
 import { handleLogin, isBrowser, getUser, setUser, isLoggedIn, isGapiAuthed, logout } from '../services/auth.js'
 
-describe('authentication wrapping library' () => {
+describe('the authentication wrapping library', () => {
   it('has an auth method', () => {
     expect(handleLogin).toBeDefined();
     expect(isBrowser).toBeDefined();
@@ -10,6 +10,10 @@ describe('authentication wrapping library' () => {
 
   it('invalidates a wrong username and password', () => {
     expect(handleLogin({username: "username", password: "password"})).toBe(false);
+  })
+
+  it('can get a user\'s empty data', () => {
+    expect(getUser()).toBeEmpty();
   })
 
   it('accepts a username and password', () => {
@@ -33,45 +37,5 @@ describe('authentication wrapping library' () => {
     const user = getUser();
     expect(user  && Object.keys(user).length === 0).toBe(true);
     expect(user.username).not.toBeDefined();
-  })
-})
-
-
-describe('Test the api wrapper and endpoints', async function() {
-  it('obtains the api methods', () => {
-    expect(getHello).toBeDefined()
-    expect(getHelloData).toBeDefined()
-    expect(postHello).toBeDefined()
-  })
-
-  it('tests against the simple promise', async () => {
-    var x = await resolveAfterABit(10)
-    expect(x).toBe(10)
-  })
-
-  it('responds at the hello world lambda stack endpoint', async () => {
-    const response = await getHello()
-    await expect(response.status).toBe(200)
-    await expect(response.json()).resolves.toMatchObject({
-      data: { greeting: 'Hello, Icarus.' },
-    })
-  })
-
-  it('posts a dynamic name to the endpoint', async function() {
-    const response = await getHelloData({
-      query: `{greeting(firstName: "Test-Runner")}`,
-    })
-    await expect(response.json()).resolves.toEqual({
-      data: { greeting: 'Hello, Test-Runner.' },
-    })
-  })
-
-  xit('accepts a posted name to the hello endpoint', async function() {
-    const response = await postHello({
-      query: `{greeting(firstName: "Icarus3")}`,
-    })
-    await expect(response.json()).resolves.toEqual({
-      data: { greeting: 'Hello, Icarus3.' },
-    })
   })
 })
